755f4418375106d7227c15b13f6e5c6e02018f31
Hansel Ong <hansel@onguniverse.com>
2014-11-24 15:32:56 -0800

    refactored briefly

--- ShoppingCartDemo/Program.cs
+++ ShoppingCartDemo/Program.cs
@@ -16,38 +16,11 @@ namespace ShoppingCartDemo
             Console.WriteLine("3. Mango $3.00");
             Console.WriteLine("Which one would you like to purchase?");
 
-            int userInput = 0;
-            try
-            {
-                userInput = Convert.ToInt16(Console.ReadLine());
-            }
-            catch
-            {
-                Console.WriteLine("Please enter a number matching the selection!");
-            }
+            int userInput = GetPurchaseOption();
 
             if (userInput > 0)
             {
-                int subtotal = 0;
-                switch (userInput)
-                {
-                    case 1:
-                        Console.WriteLine("You purchased an apple for $1.00");
-                        subtotal = 1;
-                        break;
-                    case 2:
-                        Console.WriteLine("You purchased grapes for $2.00");
-                        subtotal = 2;
-                        break;
-                    case 3:
-                        Console.WriteLine("You purchased a mango for $3.00");
-                        subtotal = 3;
-                        break;
-                    default:
-                        Console.WriteLine("Please enter a number matching the selection!");
-                        break;
-                }
-
+                int subtotal = GetSubtotal(userInput);
                 if (subtotal > 0)
                 {
                     double tax = (double)(subtotal) * (0.095);
@@ -58,6 +31,53 @@ namespace ShoppingCartDemo
 
             Console.WriteLine("Press any key to end the program.");
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Get the purchasing option from the user
+        /// </summary>
+        /// <returns>0 if user didn't give the correct option, more than 0 if user gives an int</returns>
+        static int GetPurchaseOption()
+        {
+            int userInput = 0;
+            try
+            {
+                userInput = Convert.ToInt16(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a number matching the selection!");
+            }
+            return userInput;
+        }
+
+        /// <summary>
+        /// Get the subtotal based on user's input
+        /// </summary>
+        /// <param name="userInput">user's input RE: purchasing option</param>
+        /// <returns>Subtotal based on input</returns>
+        static int GetSubtotal(int userInput)
+        {
+            int subtotal = 0;
+            switch (userInput)
+            {
+                case 1:
+                    Console.WriteLine("You purchased an apple for $1.00");
+                    subtotal = 1;
+                    break;
+                case 2:
+                    Console.WriteLine("You purchased grapes for $2.00");
+                    subtotal = 2;
+                    break;
+                case 3:
+                    Console.WriteLine("You purchased a mango for $3.00");
+                    subtotal = 3;
+                    break;
+                default:
+                    Console.WriteLine("Please enter a number matching the selection!");
+                    break;
+            }
+            return subtotal;
         }
     }
 }
