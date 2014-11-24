658ced08950c0780f8d522be718dbf1feec6b85b
Hansel Ong <hansel@onguniverse.com>
2014-11-24 15:52:49 -0800

    inventory model

---
+++ ShoppingCartDemo/Inventory.cs
@@ -0,0 +1,32 @@
+﻿namespace ShoppingCartDemo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// General class to keep track of inventory
+    /// </summary>
+    class Inventory
+    {
+        private String Name { get; set; }
+        private int Price { get; set; }
+
+        public Inventory(String name, int price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        /// <summary>
+        /// Displays the name of this inventory, complete with price
+        /// </summary>
+        /// <returns>The proper way to display this inventory</returns>
+        public String DisplayName()
+        {
+            return String.Format("{0} (${1})", this.Name, this.Price);
+        }
+    }
+}
--- ShoppingCartDemo/Program.cs
+++ ShoppingCartDemo/Program.cs
@@ -13,10 +13,16 @@ namespace ShoppingCartDemo
             int userInput;
             int subtotal = 0;
 
+            // Set up the initial inventory to purchase
+            List<Inventory> inventoryList = new List<Inventory>();
+            inventoryList.Add(new Inventory("Apple", 1));
+            inventoryList.Add(new Inventory("Grapes", 2));
+            inventoryList.Add(new Inventory("Mango", 3));
+
             // Ask the user to purchase stuff
             do
             {
-                DisplayPurchaseOptions();
+                DisplayPurchaseOptions(inventoryList);
                 userInput = GetPurchaseOption();
 
                 // Update subtotal accordingly
@@ -39,12 +45,12 @@ namespace ShoppingCartDemo
         /// <summary>
         /// Display purchase options for user
         /// </summary>
-        static void DisplayPurchaseOptions()
+        static void DisplayPurchaseOptions(List<Inventory> inventoryList)
         {
             Console.WriteLine("Please choose from the following menu:");
-            Console.WriteLine("1. Apple $1.00");
-            Console.WriteLine("2. Grapes $2.00");
-            Console.WriteLine("3. Mango $3.00");
+            Console.WriteLine("1." + inventoryList[0].DisplayName());
+            Console.WriteLine("2." + inventoryList[1].DisplayName());
+            Console.WriteLine("3." + inventoryList[2].DisplayName());
             Console.WriteLine("4. exit");
             Console.WriteLine("Which one would you like to purchase?");
         }
@@ -74,24 +80,26 @@ namespace ShoppingCartDemo
         /// <returns>Subtotal based on input</returns>
         static int GetSubtotal(int userInput, int subtotal)
         {
+            Console.Clear();
             switch (userInput)
             {
                 case 1:
                     Console.WriteLine("You purchased an apple for $1.00");
-                    subtotal = 1;
+                    subtotal += 1;
                     break;
                 case 2:
                     Console.WriteLine("You purchased grapes for $2.00");
-                    subtotal = 2;
+                    subtotal += 2;
                     break;
                 case 3:
                     Console.WriteLine("You purchased a mango for $3.00");
-                    subtotal = 3;
+                    subtotal += 3;
                     break;
                 default:
                     Console.WriteLine("Please enter a number matching the selection!");
                     break;
             }
+            Console.WriteLine();
             return subtotal;
         }
 
@@ -101,8 +109,9 @@ namespace ShoppingCartDemo
         static void DisplaySummary(int subtotal)
         {
             double tax = (double)(subtotal) * (0.095);
-            Console.WriteLine("Tax (9.5%): {0}", tax);
-            Console.WriteLine("Total: {0}", subtotal + tax);
+            Console.WriteLine("Subtotal: ${0}", subtotal);
+            Console.WriteLine("Tax (9.5%): ${0}", tax);
+            Console.WriteLine("Total: ${0}", subtotal + tax);
         }
     }
 }
