fcaa78170fbb9e2212faf6963843b2a2c922cc1c
Hansel Ong <hansel@onguniverse.com>
2014-11-24 15:40:16 -0800

    allow purchase of more items

--- ShoppingCartDemo/Program.cs
+++ ShoppingCartDemo/Program.cs
@@ -10,27 +10,43 @@ namespace ShoppingCartDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please choose from the following menu:");
-            Console.WriteLine("1. Apple $1.00");
-            Console.WriteLine("2. Grapes $2.00");
-            Console.WriteLine("3. Mango $3.00");
-            Console.WriteLine("Which one would you like to purchase?");
+            int userInput;
+            int subtotal = 0;
 
-            int userInput = GetPurchaseOption();
-
-            if (userInput > 0)
+            // Ask the user to purchase stuff
+            do
             {
-                int subtotal = GetSubtotal(userInput);
-                if (subtotal > 0)
+                DisplayPurchaseOptions();
+                userInput = GetPurchaseOption();
+
+                // Update subtotal accordingly
+                if (userInput > 0)
                 {
-                    double tax = (double)(subtotal) * (0.095);
-                    Console.WriteLine("Tax (9.5%): {0}", tax);
-                    Console.WriteLine("Total: {0}", subtotal + tax);
+                    subtotal = GetSubtotal(userInput, subtotal);
                 }
+            } while (userInput != 4);
+
+            // Display the summary of the transaction
+            if (subtotal > 0)
+            {
+                DisplaySummary(subtotal);
             }
 
             Console.WriteLine("Press any key to end the program.");
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Display purchase options for user
+        /// </summary>
+        static void DisplayPurchaseOptions()
+        {
+            Console.WriteLine("Please choose from the following menu:");
+            Console.WriteLine("1. Apple $1.00");
+            Console.WriteLine("2. Grapes $2.00");
+            Console.WriteLine("3. Mango $3.00");
+            Console.WriteLine("4. exit");
+            Console.WriteLine("Which one would you like to purchase?");
         }
 
         /// <summary>
@@ -56,9 +72,8 @@ namespace ShoppingCartDemo
         /// </summary>
         /// <param name="userInput">user's input RE: purchasing option</param>
         /// <returns>Subtotal based on input</returns>
-        static int GetSubtotal(int userInput)
+        static int GetSubtotal(int userInput, int subtotal)
         {
-            int subtotal = 0;
             switch (userInput)
             {
                 case 1:
@@ -78,6 +93,16 @@ namespace ShoppingCartDemo
                     break;
             }
             return subtotal;
+        }
+
+        /// <summary>
+        /// Display summary of the purchase
+        /// </summary>
+        static void DisplaySummary(int subtotal)
+        {
+            double tax = (double)(subtotal) * (0.095);
+            Console.WriteLine("Tax (9.5%): {0}", tax);
+            Console.WriteLine("Total: {0}", subtotal + tax);
         }
     }
 }
